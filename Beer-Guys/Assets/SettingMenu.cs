462f7d96b9364956e3746ce81390dfeb37384930
Pecin1 <elin.pecka.cz@gmail.com>
2023-10-07 13:25:02 +0200

    Option skoro dodelane

--- Beer-Guys/Assets/SettingMenu.cs
+++ Beer-Guys/Assets/SettingMenu.cs
@@ -2,13 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
     
 public class SettingMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropDown;
+
+    
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+        resolutionDropDown.AddOptions(options);
+    }
+
+    
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+    
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
 }
