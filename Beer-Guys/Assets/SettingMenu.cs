6a2cd808c313e0492f9b56f2f9b9c88479d0531f
Pecin1 <elin.pecka.cz@gmail.com>
2023-10-07 13:48:02 +0200

    Options resolution, skoro funkcni

--- Beer-Guys/Assets/SettingMenu.cs
+++ Beer-Guys/Assets/SettingMenu.cs
@@ -19,12 +19,19 @@ public class SettingMenu : MonoBehaviour
         resolutionDropDown.ClearOptions();
         
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
     }
 
     
